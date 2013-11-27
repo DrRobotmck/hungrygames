5d8fa3420129f6e3937da5390a169e1a191ee4cb
DrRobotmck <mckenneth.scott@icloud.com>
2013-11-26 20:41:53 -0500

    kids died, it was pleasant. a winner was crowned

--- hungergames/app/models/gamemaker.rb
+++ hungergames/app/models/gamemaker.rb
@@ -1,11 +1,13 @@
 class Gamemaker
-	attr_accessor :game, :district, :tributes, :rounds
+	attr_accessor :game, :district, :tributes, :rounds, :losers, :winner
 	
 	def initialize(game, district)
 		@game = game
 		@district = district
 		@tributes = []
 		@rounds = []
+		@losers = []
+		@winner = []
 	end
 
 	def select_tributes
@@ -43,6 +45,159 @@ class Gamemaker
 		@rounds.flatten
 		@tributes.each do |t|
 			t.rounds << @rounds[0]
+			t.save
 		end
 	end
+
+	def first_fight
+		round_one = Round.find(1)
+		round_two = Round.find(2)
+		round_three = Round.find(3)
+		round_four = Round.find(4)
+		round_five = Round.find(5)
+		tribs = round_one.citizens
+		12.times do
+			fight_zone = tribs.sample(2)
+			if fight_zone[0].rating > fight_zone[1].rating
+					fight_zone[1].alive = false
+					fight_zone[1].save
+					@losers<< fight_zone[1]
+					round_one.citizens.delete(fight_zone[1])
+					round_two.citizens.delete(fight_zone[1])
+					round_three.citizens.delete(fight_zone[1])
+					round_four.citizens.delete(fight_zone[1])
+					round_five.citizens.delete(fight_zone[1])
+			elsif fight_zone[1].rating > fight_zone[0].rating
+					fight_zone[0].alive = false
+					fight_zone[0].save
+					@losers << fight_zone[0]
+					round_one.citizens.delete(fight_zone[0])
+					round_two.citizens.delete(fight_zone[0])
+					round_three.citizens.delete(fight_zone[0])
+					round_four.citizens.delete(fight_zone[0])
+					round_five.citizens.delete(fight_zone[0])
+			else
+				num = rand(1)
+				fight_zone[num].alive = false
+				fight_zone[num].save
+				@losers << fight_zone[num]
+				round_one.citizens.delete(fight_zone[num])
+				round_two.citizens.delete(fight_zone[num])
+				round_three.citizens.delete(fight_zone[num])
+				round_four.citizens.delete(fight_zone[num])
+				round_five.citizens.delete(fight_zone[num])
+			end
+		end
+	end
+
+	def second_fight
+		round_two = Round.find(2)
+		round_three = Round.find(3)
+		round_four = Round.find(4)
+		round_five = Round.find(5)
+		tribs = round_two.citizens
+		6.times do
+			fight_zone = tribs.sample(2)
+			if fight_zone[0].rating > fight_zone[1].rating
+					fight_zone[1].alive = false
+					@losers<< fight_zone[1]
+					round_two.citizens.delete(fight_zone[1])
+					round_three.citizens.delete(fight_zone[1])
+					round_four.citizens.delete(fight_zone[1])
+					round_five.citizens.delete(fight_zone[1])
+			elsif fight_zone[1].rating > fight_zone[0].rating
+					fight_zone[0].alive = false
+					@losers << fight_zone[0]
+					round_two.citizens.delete(fight_zone[0])
+					round_three.citizens.delete(fight_zone[0])
+					round_four.citizens.delete(fight_zone[0])
+					round_five.citizens.delete(fight_zone[0])
+			else
+				num = rand(1)
+				fight_zone[num].alive = false
+				@losers << fight_zone[num]
+				round_two.citizens.delete(fight_zone[num])
+				round_three.citizens.delete(fight_zone[num])
+				round_four.citizens.delete(fight_zone[num])
+				round_five.citizens.delete(fight_zone[num])
+			end
+		end
+	end
+
+	def third_fight
+		round_three = Round.find(3)
+		round_four = Round.find(4)
+		round_five = Round.find(5)
+		tribs = round_three.citizens
+		3.times do
+			fight_zone = tribs.sample(2)
+			if fight_zone[0].rating > fight_zone[1].rating
+					fight_zone[1].alive = false
+					@losers<< fight_zone[1]
+					round_three.citizens.delete(fight_zone[1])
+					round_four.citizens.delete(fight_zone[1])
+					round_five.citizens.delete(fight_zone[1])
+			elsif fight_zone[1].rating > fight_zone[0].rating
+					fight_zone[0].alive = false
+					@losers << fight_zone[0]
+					round_three.citizens.delete(fight_zone[0])
+					round_four.citizens.delete(fight_zone[0])
+					round_five.citizens.delete(fight_zone[0])
+			else
+				num = rand(1)
+				fight_zone[num].alive = false
+				@losers << fight_zone[num]
+				round_three.citizens.delete(fight_zone[num])
+				round_four.citizens.delete(fight_zone[num])
+				round_five.citizens.delete(fight_zone[num])
+			end
+		end
+	end
+	def fourth_fight
+		round_four = Round.find(4)
+		round_five = Round.find(5)
+		tribs = round_four.citizens
+			fight_zone = tribs.sample(2)
+			if fight_zone[0].rating > fight_zone[1].rating
+					fight_zone[1].alive = false
+					@losers<< fight_zone[1]
+					round_four.citizens.delete(fight_zone[1])
+					round_five.citizens.delete(fight_zone[1])
+			elsif fight_zone[1].rating > fight_zone[0].rating
+					fight_zone[0].alive = false
+					@losers << fight_zone[0]
+					round_four.citizens.delete(fight_zone[0])
+					round_five.citizens.delete(fight_zone[0])
+			else
+				num = rand(1)
+				fight_zone[num].alive = false
+				@losers << fight_zone[num]
+				round_four.citizens.delete(fight_zone[num])
+				round_five.citizens.delete(fight_zone[num])
+			end
+	end
+
+	def finale
+		round_five = Round.find(5)
+		tribs = round_five.citizens
+			fight_zone = tribs.sample(2)
+			if fight_zone[0].rating > fight_zone[1].rating
+					fight_zone[1].alive = false
+					@losers<< fight_zone[1]
+					round_five.citizens.delete(fight_zone[1])
+					@winner << fight_zone[0]
+			elsif fight_zone[1].rating > fight_zone[0].rating
+					fight_zone[0].alive = false
+					@losers << fight_zone[0]
+					round_five.citizens.delete(fight_zone[0])
+					@winner << fight_zone[1]
+			else
+				num = rand(1)
+				fight_zone[num].alive = false
+				@losers << fight_zone[num]
+				round_five.citizens.delete(fight_zone[num])
+				@winner << round_five.citizens.first
+			end
+	end
+
 end
--- hungergames/spec/models/gamemaker_spec.rb
+++ hungergames/spec/models/gamemaker_spec.rb
@@ -52,8 +52,69 @@ describe Gamemaker do
 			expect(rounder.tributes.count).to eq(24)
 			# binding.pry	
 			expect(rounder.tributes[0].rounds.length).to eq(rounder.rounds[0].length)
-
 		end
-
+	
+		it "makes tributes fight" do
+			rounder.select_tributes
+			rounder.select_escort
+			rounder.starter
+			rounder.first_fight
+			expect(round[1].citizens.length).to eq(12)
+			expect(round[2].citizens.length).to eq(12)
+			expect(round[3].citizens.length).to eq(12)
+			expect(round[4].citizens.length).to eq(12)
+			expect(rounder.losers.length).to eq(12)
+			expect(rounder.losers.sample.alive).to eq(false)
+		end
+		it "makes surviving tributes fight again" do
+			rounder.select_tributes
+			rounder.select_escort
+			rounder.starter
+			rounder.first_fight
+			rounder.second_fight
+			expect(round[2].citizens.length).to eq(6)
+			expect(round[3].citizens.length).to eq(6)
+			expect(round[4].citizens.length).to eq(6)
+			expect(rounder.losers.length).to eq(18)
+			expect(rounder.losers.sample.alive).to eq(false)
+		end
+		it "makes surviving tributes fight again" do
+			rounder.select_tributes
+			rounder.select_escort
+			rounder.starter
+			rounder.first_fight
+			rounder.second_fight
+			rounder.third_fight
+			expect(round[3].citizens.length).to eq(3)
+			expect(round[4].citizens.length).to eq(3)
+			expect(rounder.losers.length).to eq(21)
+			expect(rounder.losers.sample.alive).to eq(false)
+		end
+		it "kills more tributes leaving two" do
+			rounder.select_tributes
+			rounder.select_escort
+			rounder.starter
+			rounder.first_fight
+			rounder.second_fight
+			rounder.third_fight
+			rounder.fourth_fight
+			expect(round[4].citizens.length).to eq(2)
+			expect(rounder.losers.length).to eq(22)
+			expect(rounder.losers.sample.alive).to eq(false)
+		end
+		it "crowns a winner" do
+			rounder.select_tributes
+			rounder.select_escort
+			rounder.starter
+			rounder.first_fight
+			rounder.second_fight
+			rounder.third_fight
+			rounder.fourth_fight
+			rounder.finale
+			expect(round[4].citizens.length).to eq(1)
+			expect(rounder.losers.length).to eq(23)
+			expect(rounder.losers.sample.alive).to eq(false)
+			expect(rounder.winner.length).to eq(1)
+		end
 	end
 end
